a493a30160545a032e49f19d3c6eb3150b50c256
lokeshphilips <lokesh.reddy@philips.com>
2017-10-25 16:40:35 +0530

    fe

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("hello lokesh")
+        print("hello lokesh2")
+
     }
 
     override func didReceiveMemoryWarning() {
