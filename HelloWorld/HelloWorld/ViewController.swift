d3b5f3439785a32d73ae30e9b72c62db4451ac29
lokeshphilips <lokesh.reddy@philips.com>
2017-10-25 15:42:04 +0530

    e

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Lokesh on 10/25/17.
+//  Copyright © 2017 Lokesh Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello lokesh")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
